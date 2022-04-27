1128d5853dfe15d5b1672c558f1998680bbdbcd3
Fran <fransanxenxo@gmail.com>
2022-04-27 19:29:58 +0100

    learning react, class 4: print variables in the HTML

--- react-counter/src/PrimeraApp.js
+++ react-counter/src/PrimeraApp.js
@@ -2,9 +2,17 @@ import React, { Fragment } from 'react';
 
 
 function PrimeraApp() {
+
+    const saludo ={
+        nombre : 'Fran',
+        edad: 23
+    }
+
+
     return (
         <>
-            <h1>Hola Mundo</h1>
+            <h1>{saludo.nombre}</h1>
+            <small>{saludo.edad}</small>
             <p>Mi primera aplicación</p>
         </>
     )
