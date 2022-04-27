92ab061929403ce178028a9da37fa388c510e6ba
Fran <fransanxenxo@gmail.com>
2022-04-27 19:22:18 +0100

    learning react, class 3

---
+++ react-counter/src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+
+function PrimeraApp() {
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicación</p>
+        </>
+    )
+
+}
+
+export default PrimeraApp
