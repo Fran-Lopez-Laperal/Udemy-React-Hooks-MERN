230d3e05902005038c672eb715d54477f7b659f7
Fran <fransanxenxo@gmail.com>
2022-04-27 19:54:23 +0100

    learning react, class 7: defaultPros

--- react-counter/src/PrimeraApp.js
+++ react-counter/src/PrimeraApp.js
@@ -2,21 +2,27 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types'
 
 
-function PrimeraApp({saludo}) {
+function PrimeraApp({saludo, subtitulo}) {
 
    
     return (
         <>
             <h1>{saludo}</h1>
-            <p>Mi primera aplicación</p>
+            <p>{subtitulo}</p>
         </>
     )
 
 }
 
 PrimeraApp.propTypes = {
-    saludo: PropTypes.string,
-    name: PropTypes.string.isRequired
+    saludo: PropTypes.string.isRequired
+
 }
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'soy un subtitulo'
+}
+
+
 
 export default PrimeraApp
