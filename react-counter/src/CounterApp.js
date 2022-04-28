85b91c07a195488a726357eff54d636ad4d9e74a
Fran <fransanxenxo@gmail.com>
2022-04-28 20:47:18 +0100

    feat: learning react, class 9: useState Hook

--- react-counter/src/CounterApp.js
+++ react-counter/src/CounterApp.js
@@ -1,17 +1,21 @@
-import React from "react"
+import React, { useState} from "react"
 import propTypes from 'prop-types'
 
 function CounterApp(props) {
     const { value } = props
 
+   const [counter, setCounter] = useState(0)
+
+    //handleAdd
     function handleClick(){
-        console.log(handleClick)
+    //    setCounter(counter + 1)
+    setCounter((c) => c + 1 )
     }
 
     return (
         <>
             <h1>Counter App</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
             <button onClick={handleClick}>+1</button>
         </>
     )
