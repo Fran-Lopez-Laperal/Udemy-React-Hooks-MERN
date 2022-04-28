22a6510bca1c7dfd00ddf5c03763b9b8291eecdc
Fran <fransanxenxo@gmail.com>
2022-04-28 20:31:56 +0100

    feat: learning react, class 8: nClick

--- react-counter/src/CounterApp.js
+++ react-counter/src/CounterApp.js
@@ -3,10 +3,16 @@ import propTypes from 'prop-types'
 
 function CounterApp(props) {
     const { value } = props
+
+    function handleClick(){
+        console.log(handleClick)
+    }
+
     return (
         <>
             <h1>Counter App</h1>
             <h2>{value}</h2>
+            <button onClick={handleClick}>+1</button>
         </>
     )
 }
