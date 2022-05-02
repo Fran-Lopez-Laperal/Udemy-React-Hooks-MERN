9ca460c84cec004b0f02f53212893f9935bf5045
Fran <fransanxenxo@gmail.com>
2022-05-02 16:20:34 +0100

    feat: learning react, class 21: Jest tests clicks events

--- react-counter/src/CounterApp.test.js
+++ react-counter/src/CounterApp.test.js
@@ -2,16 +2,39 @@ const { shallow } = require("enzyme")
 import CounterApp from '../src/CounterApp'
 
 describe('pruebas en el <CounterApp/> ', () => {
-    test('debe mostrar <CounterApp/> correctamente', () => {
-        
-        const wrapper = shallow(<CounterApp />)
-        expect(wrapper).toMatchSnapshot()
+
+    let wrapper = shallow(<CounterApp />)
+
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp />)
 
     })
 
+
+    test('debe mostrar <CounterApp/> correctamente', () => {
+
+        expect(wrapper).toMatchSnapshot()
+    })
+
     test('debe mostrar el valor por defecto de 100', () => {
-        const wrapper = shallow(<CounterApp value={100}/>)
-        const counterText = wrapper.find('h2').text()
+
+        const wrapper = shallow(<CounterApp value={100} />)
+        const counterText = wrapper.find('h2').text().trim()
         expect(counterText).toBe('100')
+    })
+
+
+    test('debe incrementar con el boton +1', () => {
+
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('11');
+    })
+
+    test('debe decrementar con el boton -1', () => {
+
+        wrapper.find('button').at(2).simulate('click')
+        const counterText = wrapper.find('h2').text().trim()
+        expect(counterText).toBe('9')
     })
 })
