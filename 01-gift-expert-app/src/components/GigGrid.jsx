2638f6fc374c7e201c9029e2f5e519c2b5ff88fe
Fran <fransanxenxo@gmail.com>
2022-05-03 20:36:10 +0100

    feat: class 4: useEffect

--- 01-gift-expert-app/src/components/GigGrid.jsx
+++ 01-gift-expert-app/src/components/GigGrid.jsx
@@ -1,5 +1,13 @@
+import { useState, useEffect } from "react";
+
 function GifGrid({ category }) {
 
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        getGifs()
+    },[])
+    //array de dependencias, esto 
     const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=YmFkzSuZrFRq1U7GgSZd9gCuRYSZCwMk'
         const resp = await fetch( url );
@@ -14,10 +22,12 @@ function GifGrid({ category }) {
         })
         console.log(gifs)
     }
-    getGifs()
+    // getGifs()
     return (
         <div>
             <h3>{category}</h3>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}></button>
         </div>
     )
 }
