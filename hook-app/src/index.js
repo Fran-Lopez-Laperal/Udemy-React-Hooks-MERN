e1d055ca15b4283c6acefccb018ba9d4288166e0
Fran <fransanxenxo@gmail.com>
2022-05-16 12:24:08 +0100

    feat: class 32: useMemo()

--- hook-app/src/index.js
+++ hook-app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MemoHook from './components/06-Memos/MemoHook';
 // import MultipleCustomHook from './components/03-examples/MultipleCustomHooks';
 // import FocusScreen from './components/04-useRef/FocusScreen';
 // import RealExampleRef from './components/04-useRef/RealExampleRef';
 // import Layout from './components/05-useLayoutEffect/layout';
-import Memorize from './components/06-Memos/Memorize';
+// import Memorize from './components/06-Memos/Memorize';
 // import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 // import CounteWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
@@ -15,7 +16,7 @@ import Memorize from './components/06-Memos/Memorize';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Memorize/>
+  <MemoHook/>
   </React.StrictMode>
 );
 
